cba5c3a88eb5af1c5cc32cae0b614e0f01235d4a
philip ritchey <pcr@tamu.edu>
2026-02-06 16:03:42 -0600

    extract test function, add colorful text

--- week_4/solve_quadratic/test_quadratic.cpp
+++ week_4/solve_quadratic/test_quadratic.cpp
@@ -26,31 +26,41 @@ std::ostream& operator<<(std::ostream& output_stream, const vector<T>& v) {
     return output_stream;
 }
 
-int main() {
-    // test_solve_quadratic({1, 0, -1}, {1, -1});
-    {
-        // x^2 - 1 = 0
-        // 1x^2 + 0x + -1 = 0
-        // (x+1)(x-1) = 0
-        // should have have 2 roots: 1, -1
-        vector<double> expected_roots = {1, -1};
-        vector<double> actual_roots = solve_quadratic(1, 0, -1);
-        cout << "expected: " << expected_roots << endl;
-        cout << "  actual: " << actual_roots << endl;
-        assert(actual_roots == expected_roots);
-    }
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string RESET = "\033[0m";
 
-    {
-        // x^2 - 4 = 0
-        // 1x^2 + 0x + -4 = 0
-        // (x+2)(x-2) = 0
-        // should have have 2 roots: 2, -2
-        vector<double> expected_roots = {2, -2};
-        vector<double> actual_roots = solve_quadratic(1, 0, -4);
-        cout << "expected: " << expected_roots << endl;
-        cout << "  actual: " << actual_roots << endl;
-        assert(actual_roots == expected_roots);
+void test_solve_quadratic(const vector<double>& coefficients, const vector<double>& expected_roots) {
+    double a = coefficients.at(0);
+    double b = coefficients.at(1);
+    double c = coefficients.at(2);
+    vector<double> actual_roots = solve_quadratic(a, b, c);
+    if (actual_roots == expected_roots) {
+        cout << GREEN
+             << "[PASS] correct roots for " << coefficients << endl
+             << RESET;
+    } else {
+        cout << RED
+             << "[FAIL] incorrect roots for " << coefficients << endl
+             << "  expected: " << expected_roots << endl
+             << "    actual: " << actual_roots << endl
+             << RESET;
+        exit(1);
     }
+}
+
+int main() {
+    // x^2 - 1 = 0
+    // 1x^2 + 0x + -1 = 0
+    // (x+1)(x-1) = 0
+    // should have have 2 roots: 1, -1
+    test_solve_quadratic({1, 0, -1}, {1, -1});
+
+    // x^2 - 4 = 0
+    // 1x^2 + 0x + -4 = 0
+    // (x+2)(x-2) = 0
+    // should have have 2 roots: 2, -2
+    test_solve_quadratic({1, 0, -4}, {2, -2});
 
     {
         // x^2 = 0
@@ -96,16 +106,18 @@ int main() {
         assert(actual_roots == expected_roots);
     }
 
+    // TODO(student): decide how to handle case of 0 = 0
+    //                what is the expected behavior?
     {
         // 0 = 0
         // 0x^2 + 0x + 0 = 0
-        // should have have ? roots: ?  TODO(student)
-        vector<double> expected_roots = {};
+        // should have have ? roots: ?
+        vector<double> expected_roots = {};  // TODO(student): change this value
         vector<double> actual_roots = solve_quadratic(0, 0, 0);
         cout << "expected: " << expected_roots << endl;
         cout << "  actual: " << actual_roots << endl;
         assert(actual_roots == expected_roots);
     }
 
-    cout << "ALL TESTS PASSING" << endl;
+    cout << GREEN << "ALL TESTS PASSING" << RESET << endl;
 }
