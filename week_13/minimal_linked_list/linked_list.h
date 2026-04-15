a91e51da8283abaa011425536a2ae1ab51a20d0c
philip ritchey <pcr@tamu.edu>
2026-04-15 14:44:03 -0500

    add initial sketch of minimal linked list design

---
+++ week_13/minimal_linked_list/linked_list.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// TODO(pcr): implement a minimal linked list
+
+// Item
+//   + value: T
+//   + next: pointer to Item
+//   + (constructor)
+
+// LinkedList
+//   - head: pointer to Item
+//   + (constructor)
+//   + empty() -> bool
+//   + add(value: T)
+//   + front() -> T
+//   + remove()
