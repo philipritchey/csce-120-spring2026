22ee1c7e3403885e36fa7a3ef966b3d3cb8f3ebf
philip ritchey <pcr@tamu.edu>
2026-04-20 16:39:08 -0500

    add Fox

---
+++ week_14/demo/demo3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using std::cout, std::endl;
+
+#include <string>
+using std::string;
+
+class Animal {
+ public:
+    virtual string talk() const { return "<animal noises>"; }
+};
+
+class Pig : public Animal {
+ public:
+    string talk() const override { return "oink"; }
+};
+
+class Duck : public Animal {
+ public:
+    string talk() const override { return "quack"; }
+};
+
+class Horse : public Animal {
+ public:
+    string talk() const override { return "neigh"; }
+};
+
+class Fox : public Animal {
+ public:
+    string talk() const override { return "ring ding ding"; }
+};
+
+string talk_to(Animal const& animal) {
+    return animal.talk();
+}
+
+int main() {
+    Pig pig;
+    Duck duck;
+    Horse horse;
+    Fox fox;
+
+    cout << "The pig says: " << talk_to(pig) << endl;
+    cout << "The duck says: " << talk_to(duck) << endl;
+    cout << "The horse says: " << talk_to(horse) << endl;
+    cout << "The fox says: " << talk_to(fox) << endl;
+}
